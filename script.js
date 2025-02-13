c9fbb207cd6308e05f78e1b522ab147086a3a3f1
Jud1th1 <127470572+Jud1th1@users.noreply.github.com>
2025-02-12 21:17:43 -0500

    AdvancedSlide show

---
+++ script.js
@@ -0,0 +1,5 @@
+var currentImage = 0;
+var myPhotos = ['slides/image1.jpg','slides/image2.jpg','slides/image3.jpg','slides/image4.jpg', 'slides/image5.jpg'];
+var container = document.getElementById('content');
+var nextBtn = document.getElementById('next');
+var prevBtn = document.getElementById('previous');
