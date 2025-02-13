43c159eabb664d5b4b1cccce8cee43f7c8963e8e
Jud1th1 <127470572+Jud1th1@users.noreply.github.com>
2025-02-12 21:59:44 -0500

    finished script and clean it up :)

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-var currentImage = 0;
-var myPhotos = ['slides/image1.jpg','slides/image2.jpg','slides/image3.jpg','slides/image4.jpg', 'slides/image5.jpg'];
-var container = document.getElementById('content');
-var nextBtn = document.getElementById('next');
-var prevBtn = document.getElementById('previous');
+(function(){
+    "use strict";
+
+    let currentImage = 0;
+    const myPhotos = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg', 'image5.jpg'];
+    const container = document.getElementById('content');
+    const nextBtn = document.getElementById('next');
+    const prevBtn = document.getElementById('previous');
+
+    nextBtn.addEventListener('click', function(event){
+        event.preventDefault();
+
+        currentImage++;
+        if(currentImage > myPhotos.length - 1){
+            currentImage = 0;
+        } 
+
+        swapImage();
+    });
+
+
+
+    prevBtn.addEventListener('click', function(event){
+        event.preventDefault();
+
+        currentImage--;
+        if(currentImage < 0){
+            currentImage = myPhotos.length-1;
+        }
+
+        swapImage();
+    });
+
+    function swapImage(){
+        const newSlide = document.createElement('img'); //Create a new image element
+        newSlide.src = `slides/${myPhotos[currentImage]}`;//Set the source attribute
+        newSlide.className = 'fadeinimg';//Set the class name
+        container.appendChild(newSlide);//Append it to the container 
+
+        //Remove extra child
+        if(container.children.length > 2){
+            container.removeChild(container.children[0]); //removes bottom most child from list
+        }
+    }
+})();
+
